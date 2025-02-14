6a347d6b301e5366b1507f0c08a292b4ddb12cf3
Kaszniam <mkasznia95@gmail.com>
2025-02-14 12:55:21 +0100

    [F-6] Add register backend logic.

--- backend/public/api.php
+++ backend/public/api.php
@@ -9,14 +9,33 @@ header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: *");
 
 require 'functions.php';
+require 'database.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-if ($method === 'POST') {
-    sendJsonResponse(["message" => "Post works!"]);
+$request = isset($_GET['request']) ? explode('/', trim($_GET['request'], '/')) : [];
+
+$db = new Database();
+
+if ($request[0] === "register" && $method === "POST") {
+
+    $data = json_decode(file_get_contents("php://input"), true);
+    $email = $data["email"];
+    $pwd = $data["password"];
+    if($db->createUser($email, $pwd)){
+        sendJsonResponse(["message" => "User created successfully!"]);
+    }else{
+        sendJsonResponse(["message" => "User creation failed!"]);
+    }
 }
-if ($method === 'GET') {
-    sendJsonResponse(["message" => "Get works!"]);
+else if ($request[0] === "register" && $method === "GET") {
+    sendJsonResponse(["message" => "Register GET works!"]);
+}else if ($request[0] === "login" && $method === "GET") {
+     sendJsonResponse(["message" => "Login GET works!"]);
+}else if ($request[0] === "login" && $method === "POST") {
+     sendJsonResponse(["message" => "Login Post works!"]);
+}else{
+    sendJsonResponse(["message" => "No such endpoint!"]);
 }
 
 ?>
---
+++ backend/public/database.php
@@ -0,0 +1,28 @@
+<?php
+class Database {
+    private $pdo;
+
+    public function __construct() {
+        $host = "mysql-db";
+        $dbname = "todos_db";
+        $username = "todo_user";
+        $password = "todopass";
+
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
+
+    public function createUser($email, $password) {
+        $dbQuery = $this->pdo->prepare("INSERT INTO users (email, password) VALUES (?, ?)");
+        return $dbQuery->execute([$email, $password]);
+    }
+}
+?>
